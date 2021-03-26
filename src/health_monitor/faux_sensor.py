200ee0f32f137822fcc943f8b34624f1754017c1
Ben Kolligs <bkolligs@andrew.cmu.edu>
2021-03-26 10:41:50 -0400

    created health monitor node and sensor test node

---
+++ src/health_monitor/faux_sensor.py
@@ -0,0 +1,45 @@
+import rospy
+from dragoon_messages.msg import watchHeartbeat
+
+class fauxSensor(object):
+    def __init__(self):
+        # this is the name of the sensor to imitate, that will go in the 'node_name' field
+        self.sensorImitation_ = str(rospy.get_param("faux_sensor_imitation", "lidar"))
+        # length of heartbeat duration in heartbeats published
+        self.heartbeatDuration_ = int(rospy.get_param("faux_sensor_duration", "100"))
+        self.timerFreq_ = float(rospy.get_param('~timerFreq_', '10'))
+        self.nodeName_ = "faux_" + self.sensorImitation_ + "_publisher"
+
+        self.heartbeatsPublished_ = 0
+        self.heartbeatMsg_ = watchHeartbeat()
+        self.sensorPublisher_ = rospy.Publisher(
+            "/test_heartbeat",
+            watchHeartbeat,
+            queue_size=5,
+        )
+
+    def pub(self):
+        # msg type contains information to restart node
+        self.heartbeatMsg_.node_pkg = "health_monitor"
+        # node file
+        self.heartbeatMsg_.node_type = "faux_sensor.py"
+        # node name
+        self.heartbeatMsg_.node_name = self.nodeName_
+        if self.heartbeatsPublished_ < self.heartbeatDuration_:
+            self.sensorPublisher_.publish()
+    
+    def run(self):
+        rospy.init_node(self.nodeName_)
+        while not rospy.is_shutdown():
+            self.pub
+            rospy.spin()
+            rospy.sleep(1.0/self.timerFreq_)
+
+# main run loop
+if __name__ == "__main__":
+    node = fauxSensor()
+
+    try:
+        node.run()    
+    except rospy.ROSInitException:
+        pass
---
+++ src/health_monitor/health_monitor.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+
+import rospy
+from std_msgs.msg import String, Bool, Int32, Float64
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import TwistWithCovarianceStamped
+from sensor_msgs.msg import Imu
+from dragoon_messages.msg import watchHeartbeat
+
+
+class HealthMonitor():
+    def __init__(self):
+        # declare params, subscriber and publisher
+        self.myVar1_ = int(rospy.get_param('~myVar1_', '1'))
+        self.myVar2_ = float(rospy.get_param('~myVar2_', '1.0'))
+        self.timerFreq_ = float(rospy.get_param('~timerFreq_', '20'))
+
+        self.imuSub_ = rospy.Subscriber('/imu/data', Imu, self.imuCallback)
+        self.odomSub_ = rospy.Subscriber('/odom', Odometry, self.odomCallback)
+
+        self.vehicleCmdPub_ = rospy.Publisher(
+            '/vehicle_cmd', TwistWithCovarianceStamped, queue_size=5)
+
+        # declare member variables
+        self.currImu_ = Imu()
+        self.currVel_ = 0.0
+        self.vehicleCmdMsg_ = TwistWithCovarianceStamped()
+
+        while not rospy.is_shutdown():
+            self.run()
+            rospy.spin()
+            rospy.sleep(1.0/self.timerFreq_)
+
+    def run(self):
+        # do something cool
+        self.vehicleCmdMsg_.twist.twist.linear.x = self.currVel_
+        self.vehicleCmdPub_.publish(self.vehicleCmdMsg_)
+
+    def imuCallback(self, msg):
+        self.currImu_ = msg
+
+    def odomCallback(self, msg):
+        self.currVel_ = msg.twist.twist.linear.x
+
+
+if __name__ == 'main':
+    rospy.init_node('health_monitor')
+    try:
+        node = HealthMonitor()
+    except rospy.ROSInitException:
+        pass
